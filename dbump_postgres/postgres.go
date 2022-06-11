d817a1a6801b80e0c7ba6e2c54da35c68d82ffdf
local dev <>
2022-06-11 22:55:03 +0300

    Added package with std *sql.DB

--- dbump_postgres/postgres.go
+++ dbump_postgres/postgres.go
@@ -1,24 +1,26 @@
-package dbump_pgx
+package dbump_postgres
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 
 	"github.com/cristalhq/dbump"
-	"github.com/jackc/pgx/v4"
 )
 
 // to prevent multiple migrations running at the same time
-const lockNum int64 = 777_777_777
+const lockNum int64 = 707_707_707
 
 var _ dbump.Migrator = &Migrator{}
 
 // Migrator to migrate Postgres.
 type Migrator struct {
-	conn *pgx.Conn
+	conn *sql.DB
 }
 
 // NewMigrator instantiates new Migrator.
-func NewMigrator(conn *pgx.Conn) *Migrator {
+// Takes std *sql.DB.
+func NewMigrator(conn *sql.DB) *Migrator {
 	return &Migrator{
 		conn: conn,
 	}
@@ -30,26 +32,26 @@ func (pg *Migrator) Init(ctx context.Context) error {
 	version    BIGINT                   NOT NULL PRIMARY KEY,
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL
 );`
-	_, err := pg.conn.Exec(ctx, query)
+	_, err := pg.conn.ExecContext(ctx, query)
 	return err
 }
 
 // LockDB is a method for Migrator interface.
 func (pg *Migrator) LockDB(ctx context.Context) error {
-	_, err := pg.conn.Exec(ctx, "SELECT pg_advisory_lock($1);", lockNum)
+	_, err := pg.conn.ExecContext(ctx, "SELECT pg_advisory_lock($1);", lockNum)
 	return err
 }
 
 // UnlockDB is a method for Migrator interface.
 func (pg *Migrator) UnlockDB(ctx context.Context) error {
-	_, err := pg.conn.Exec(ctx, "SELECT pg_advisory_unlock($1);", lockNum)
+	_, err := pg.conn.ExecContext(ctx, "SELECT pg_advisory_unlock($1);", lockNum)
 	return err
 }
 
 // Version is a method for Migrator interface.
 func (pg *Migrator) Version(ctx context.Context) (version int, err error) {
 	query := "SELECT COUNT(*) FROM _dbump_schema_version;"
-	row := pg.conn.QueryRow(ctx, query)
+	row := pg.conn.QueryRowContext(ctx, query)
 	err = row.Scan(&version)
 	return version, err
 }
@@ -60,14 +62,26 @@ func (pg *Migrator) SetVersion(ctx context.Context, version int) error {
 VALUES ($1, NOW())
 ON CONFLICT (version) DO UPDATE
 SET created_at = NOW();`
-	_, err := pg.conn.Exec(ctx, query, version)
+	_, err := pg.conn.ExecContext(ctx, query, version)
 	return err
 }
 
 // Exec is a method for Migrator interface.
 func (pg *Migrator) Exec(ctx context.Context, query string, args ...interface{}) error {
-	return pg.conn.BeginTxFunc(ctx, pgx.TxOptions{}, func(tx pgx.Tx) error {
-		_, err := tx.Exec(ctx, query, args...)
-		return err
-	})
+	tx, err := pg.conn.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("begin transaction: %w", err)
+	}
+
+	_, err = tx.ExecContext(ctx, query, args...)
+	if err != nil {
+		_ = tx.Rollback()
+		return fmt.Errorf("exec query: %w", err)
+	}
+
+	if err = tx.Commit(); err != nil {
+		return fmt.Errorf("commit transaction: %w", err)
+	}
+
+	return nil
 }
